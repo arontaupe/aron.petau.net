9d1bbbf5104f24784867a81fd957a8ff8189f7c7
arontaupe <aron@petau.net>
2024-04-05 18:12:22 +0200

    prebuild site

---
+++ _site/README.md
@@ -0,0 +1,8 @@
+# My Portfolio
+
+Here I host some of my recent work and share it with y'all. Feel free to have a look around.
+
+The Portfolio is built around a static website generator based on ruby. It uses the `jekyll` gem to generate the website. The website is hosted on GitHub Pages.
+
+This enables me to generate all website entries from markdown files. This way I can easily add new projects and blog posts without having to write HTML.
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,139 @@
+var store = [{
+        "title": "Philosophy",
+        "excerpt":"Critical considerations during my studies I have attended a fair share of philosophical seminars in my studies and consider it a core topic connected both to science and to digital environments. Normative and feminist social theory, as well as the theory of science and phenomenology, are all brought to me...","categories": [],
+        "tags": ["ethics","philosophy of emotions","postphenomenology","values in science","private","judith butler","elizabeth anderson","josé medina","francois ewald","miranda fricker","elsa dorlin","michael foucault","alison jaggar","philosophy","feminism","epistemology","phenomenology","ethics","normativity","university of osnabrück"],
+        "url": "/philosophy/",
+        "teaser": "/assets/images/grass_blur.jpg"
+      },{
+        "title": "3D printing",
+        "excerpt":"3D Printing 3D Printing is more than just a hobby for me In it, I see societal changes, the democratization of production, and creative possibilities. Plastic does not have to be one of our greatest environmental problems if we just choose to change our perspective and behavior toward it. Plastic...","categories": [],
+        "tags": ["accessibility","creality","decentral","democratic","engineering","experiment","gcode","octoprint","parametric design","plastics","prusa","slicing","private","work","additive manufacturing","3D printing","university of osnabrück"],
+        "url": "/printing/",
+        "teaser": "/assets/images/prusa.jpg"
+      },{
+        "title": "3D Modeling and CAD",
+        "excerpt":"3D Modeling and CAD Designing 3D Objects While learning about 3D Printing, I was most intrigued by the possibility to modify and repair existing products. While there is an amazing community with lots of good and free models around, naturally I came to a point where I did not find...","categories": [],
+        "tags": ["sketchfab","fusion360","functional design","design for printing","private","photogrammetry","scaniverse","virtual reality","3D printing","polycam","parametric modelling","university of osnabrück","work"],
+        "url": "/cad/",
+        "teaser": "/assets/images/render_bike_holder.png"
+      },{
+        "title": "BEACON",
+        "excerpt":"BEACON: Decentralizing the Energy Grid in inaccessible and remote regions Access to Electricity is a basic human right. At first, that may seem over the top, but if one stops to think what all the little tasks that electricity can indirectly handle for us (lightning, laundry, cooking, freezing, heating, entertaining…)...","categories": [],
+        "tags": ["decentral","democratic","python","engineering","data viz","electricity","solar","data collection","simulation","grid","scaling","himalaya","india","research","energy","university of osnabrück","iit kharagpur"],
+        "url": "/beacon/",
+        "teaser": "/assets/images/india_key_monastery.jpg"
+      },{
+        "title": "Plastic Recycling",
+        "excerpt":"Being involved with 3D Printers, there is the issue of sustainability that I am confronted with regularly. Most 3D printed parts never get recycled and add to the global waste problem, rather than reducing it. The printer most certainly doesn’t care what it is printing, the main problem is the...","categories": [],
+        "tags": ["decentral","democratic","precious plastic","plastics","recycling","circular","cradle-to-cradle","automatic","ml","Arduino","Linux","filastruder","private","master thesis","waste","sustainability","environment","3D printing","filament","shredder"],
+        "url": "/plastic-recycling/",
+        "teaser": "/assets/images/recycling_graphic.jpg"
+      },{
+        "title": "Ballpark",
+        "excerpt":"Ballpark: 3D Environments in Unity Implemented in Unity, Ballpark is a Concept work for a collaborative 2-Player Game, where one player is a navigator with a third-person perspective and another player is a copilot, responsible for interaction with the environment – featuring mostly working physics, intelligent enemies, a gun, a...","categories": [],
+        "tags": ["3D graphics","Unity","C#","cyberpunk","collaborative","game","physics","communication","1st person","3rd person","2 player","university of osnabrück"],
+        "url": "/ballpark/",
+        "teaser": "/assets/images/ballpark_menu.png"
+      },{
+        "title": "Coding Examples",
+        "excerpt":"Neural Networks and Computer Vision A selection of coding projects Although pure coding and debugging are often not a passion of mine, I recognize the importance of neural networks and other recent developments in Computer Vision. From several projects regarding AI and Machine Learning that I co-authored during my Bachelor...","categories": [],
+        "tags": ["ethics","computer vision","neural nets","face detection","object recognition","GOFAI","super resolution","jupyter notebook","google colab","python","tensorflow","keras","machine learning","AI","MTCNN","CNN","university of osnabrück"],
+        "url": "/coding/",
+        "teaser": "/assets/images/sample_lr.png"
+      },{
+        "title": "Homebrew",
+        "excerpt":"Brewing Making my own beer I love hosting, I love experimenting in the Kitchen. Starting with homebrews was a natural fit for me and during the first wave of Covid-19, I went the whole homebrewer’s route of bottle fermentation and small batches later elevating my game with larger batches of...","categories": [],
+        "tags": ["experiment","beer","homebrew","private","lager","altbier","hops","keg","fermentation","pressure","yeast","sustainability"],
+        "url": "/homebrew/",
+        "teaser": "/assets/images/beer_tap.jpg"
+      },{
+        "title": "Chatbot",
+        "excerpt":"Guru to Go: a speech-controlled meditation assistant and sentiment tracker Here, you see a Demo video of a voice-controlled meditation assistant that we worked on in the course “Conversational Agents and speech interfaces” Course Description The central goal of the entire project was to make the Assistant be entirely speech...","categories": [],
+        "tags": ["nlp","nlu","google assistant","python","speech interface","data viz","work","sql","voice assistant","meditation","chatbot","google dialogflow","google cloud","university of osnabrück"],
+        "url": "/chatbot/",
+        "teaser": "https://cloud.google.com/dialogflow/es/docs/images/fulfillment-flow.svg"
+      },{
+        "title": "Bachelor Thesis",
+        "excerpt":"An online psycholinguistic study using reaction time Last year, I wrote my thesis during the pandemic. With the struggles our university had transitioning to online teaching, I selected a guided topic, although my initial dream was to start writing about my proposed plan for automated plastic recycling. You can read...","categories": [],
+        "tags": ["audiovisual asynchrony","autism","javascript","latency","latex","multi-sensory integration","pavlovia","psychoJS","psycholinguistics","python","r","reaction time","seaborn","sensory hypersensitivity","smart hearing protection","thesis","university of osnabrück"],
+        "url": "/thesis/",
+        "teaser": "/assets/images/rt_choice_corr_by_condition.png"
+      },{
+        "title": "Iron Smelting",
+        "excerpt":"Iron Smelting Impressions from the International Smelting Days 2021 The concept Since I was a small child I regularly took part in the yearly international congress called Iron Smelting Days (ISD). This is a congress of transdisciplinary people from all over Europe, including historians, archeologists, blacksmiths, steel producers, and many...","categories": [],
+        "tags": ["experiment","archeology","iron smelting","private","technology","history","ISD","iron age","furnace","bloomery","iron","ore","coal","clay","private"],
+        "url": "/iron-smelting/",
+        "teaser": "/assets/images/compacting_iron.jpg"
+      },{
+        "title": "Ällei",
+        "excerpt":"Meet Ällei - the accessible chatbot Sommerblut Natural Language Understanding fascinates me and recently I started collaborating with the team of the Sommerblut Festival in Cologne to deliver them a customized chatbot that will be able to communicate with everyone, respecting accessibility standards to include all people. It will be...","categories": [],
+        "tags": ["nlp","nlu","google assistant","ibm watson assistant","speech interface","backend web programming","rest api","python","inclusivity","sign language","screen reader","work","sommerblut","google dialogflow","google cloud"],
+        "url": "/allei/",
+        "teaser": "/assets/images/allei_screenshot.png"
+      },{
+        "title": "Lusatia - an immersion in (De)Fences",
+        "excerpt":"             On an Excursion to Lusatia, a project with the Working Title (De)Fences was born. Here are the current materials.     TODO: upload unity project  ","categories": [],
+        "tags": ["lusatia","coal","energy","climate","environment","barriers","fences","borders","exploitation","unity","agisoft metashape","photogrammetry","drone","tempelhofer feld","studio d+c","university of the arts berlin","udk"],
+        "url": "/lusatia/",
+        "teaser": "/assets/images/lusatia/lusatia_excavator.jpg"
+      },{
+        "title": "Lampshades",
+        "excerpt":"Lampshades During 2022, I was exposed to some of the awesomenest tools for architects. One of them was Rhino, a 3D modeling software that is used for a lot of architectural design. I hate it. It has quite an unreadable interface and is not very intuitive, with straight-up 80s vibes....","categories": [],
+        "tags": ["rhino","grasshopper","parametric","lamp","lampshade","private","3D printing","studio d+c","university of the arts berlin","udk","TODO, unfinished"],
+        "url": "/lampshades/",
+        "teaser": "/assets/images/lampshades/lampshade4.heic"
+      },{
+        "title": "Auraglow",
+        "excerpt":"What makes a room? How do moods and atmospheres emerge? Can we visualize them to make the experiences visible? The project “The Nature of Objects” aims to expand (augment) perception by making the moods of places tangible through the respective auras of the objects in the space. What makes objects...","categories": [],
+        "tags": ["journal","unity","ar","magic leap","aura","image recognition","particle systems","feng shui","relations","hand recognition","aruco","light tracking","studio d+c","university of the arts berlin","udk"],
+        "url": "/auraglow/",
+        "teaser": "/assets/images/cage_closeup.jpeg"
+      },{
+        "title": "Ruminations",
+        "excerpt":"Ruminations was a contemplation on data privacy at Amazon. It asks how to subvert browser fingerprinting and evading the omnipresent tracking of the consumer. The initial idea was to somehow, by interacting with the perpetrator and letting data accumulate that would degrade their knowledge and thereby destroy predictablity, making this...","categories": [],
+        "tags": ["journal","javascript","computer vision","data privacy","capitalism","pattern recognition","image classifier","consumerism","browser fingerprinting","amazon","data privacy","data","privacy","studio d+c","university of the arts berlin","udk","TODO, unfinished"],
+        "url": "/ruminations/",
+        "teaser": "/assets/images/ruminations/ruminations1.jpeg"
+      },{
+        "title": "Ascendancy",
+        "excerpt":"Ascendancy Ascendancy was an exploration of hacking states. Pirate Nations and Micronations have a rich history of challenging and ridiculing the concept of a nation state. Meet ascendancy, the portable, autonomous and self-moving state. Within the great nation of ascendancy, a Large language nodel (that is of course confined to...","categories": [],
+        "tags": ["borders","nation","micronation","gpt4all","text-to-speech","mastodon","fences","barriers","politics of design","technische universität berlin","TODO, unfinished"],
+        "url": "/ascendancy/",
+        "teaser": "/assets/images/ascendancy.jpg"
+      },{
+        "title": "Stable Dreamfusion",
+        "excerpt":"Stable Dreamfusion Sources I forked a really popular implementation that reverse engineered the Google Dreamfusion algorithm. This algorithm is closed-source and not publicly available. The implementation I forked is here This one is running on stable-diffusion as a bas process, which means we are are expected to have worse results...","categories": [],
+        "tags": ["dreamfusion","ai","3D graphics","mesh","generative","studio d+c","university of the arts berlin","udk","TODO, unfinished"],
+        "url": "/stable-dreamfusion/",
+        "teaser": "/assets/images/dreamfusion/sd_pig.png"
+      },{
+        "title": "Autoimmunitaet",
+        "excerpt":"How do we design our Commute? In the context of the Design and Computation Studio Course Milli Keil, Marla Gaiser and me developed a concept for a playful critique of the traffic decisions we take and the idols we embrace. It should open up questions of whether the generations to...","categories": [],
+        "tags": ["suv","interactive","cars","last generation","3D printing","action figure","aufstandlastgen","studio d+c","university of the arts berlin","udk"],
+        "url": "/autoimmunitaet/",
+        "teaser": "/assets/images/autoimmunitaet/autoimmunitaet-1.jpg"
+      },{
+        "title": "Dreams of Cars",
+        "excerpt":"Photography In the context of the course “Fotografie Elementar” with Sebastian Herold I developed a small concept of urban intervention. The results were exhibited at the UdK Rundgang 2023 and are also visible here. Dreams of Cars These are not just cars. They are Sport Utility Vehicles. What might they...","categories": [],
+        "tags": ["photography","suv","greenscreen","lightroom","photoshop","imaginaries","cars","ads","dreams","urban intervention","UdK Berlin"],
+        "url": "/dreams-of-cars/",
+        "teaser": "/assets/images/suv/suv_door-1.jpg"
+      },{
+        "title": "Postmaster",
+        "excerpt":"Postmaster Hello from aron@petau.net! Background Emails are a wondrous thing and I spend the last weeks digging a bit deeper in how they actually work. Some people consider them the last domain of the decentralized dream the internet once had and that is now popping up again with federation and...","categories": [],
+        "tags": ["server","web","petau.net","dev-ops","open protocols","federation","peer-to-peer","email","activitypub"],
+        "url": "/postmaster/",
+        "teaser": null
+      },{
+        "title": "Commoning Cars",
+        "excerpt":"Commoning cars TCF Project Brief This Project was conceptualized durin a 2023 Workshop titled Tangible Climate Futures. Aron Petau aron@petau.net See the Project in Realtime Title Making Cars Public spaces Commoning Cars Abstract Cars bad. Cars occupy public spaces resulting un a factual privatization of public goods/infrastructure. What if cars...","categories": [],
+        "tags": ["war on cars","public spaces","commons","urban intervention","UdK Berlin","private","ars electronica","accessibility activism"],
+        "url": "/commoning-cars/",
+        "teaser": null
+      },{
+        "title": "AIRASPI Build Log",
+        "excerpt":"AI-Raspi Build Log This should document the rough steps to recreate airaspi as I go along. Rough Idea: Build an edge device with image recognition and object detection capabilites. It should be realtime, aiming for 30fps at 720p. Portability and usage at installations is a priority, so it has to...","categories": [],
+        "tags": ["local AI","coral","raspberry pi","edge TPU","docker","frigate","private","surveillance","edge computing"],
+        "url": "/airaspi-build-log/",
+        "teaser": null
+      }]
